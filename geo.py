b9d8f64a077f02cff7ef038c044cbce04a6ed20f
Caroldm <caroline.monteiro@mssm.edu>
2015-12-11 12:08:36 -0500

    Update geo.py
    Changed descriptions, removed password.

--- geo.py
+++ geo.py
@@ -1,4 +1,4 @@
-#1) Call perl script and execute it (have to keep it in the same folder-path)
+# Call perl script and execute it.
 import subprocess, sys
 
 perl = "/usr/bin/perl"
@@ -7,12 +7,13 @@ params = "--mount-doom-hot"
 pl_script = subprocess.Popen([perl, perl_script, params], stdout=sys.stdout)
 pl_script.communicate()
 
-## 2) input the output from the perl script but only the wanted data.
+# Use the perl output as input. 
+
 import re
 
+# Delete
 del_list = ['>','title', 'taxon','PDAT','<','Type="String"','Item Name=','/Item','"','</Item>','<DocSum>','<Item Name=','DocSum','Accession']
-
-# I want to keep the rest of the line but not these words.
+# Lines with
 words = ['"Accession" Type="String">GSE','<Item Name="title" Type="String">','taxon','<Item Name="PDAT" Type="String">']
 
 rep = re.compile(r'|'.join(del_list))
@@ -25,32 +26,32 @@ with open("geo.txt") as f, open("email_data.txt","w") as out:
         if keep.search(line):
             # replace all unwanted substrings
             line = rep.sub("", line.lstrip())
-            line = r3.sub("\n"'http://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE', line)
+            line = r3.sub("\n"'http://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE', line)  # Change Accesion number to Accession hyperlink.
             out.write(line)
 
-### 6) Send results by email
+# Email
 from email.MIMEMultipart import MIMEMultipart
 from email.MIMEText import MIMEText
 
-fromaddr = "geo.weekly.analysis@gmail.com"
-toaddr = ""
+fromaddr = "geo.weekly.analysis@gmail.com" # sender email
+toaddr = ""           # receiver email
 msg = MIMEMultipart()
 msg['From'] = fromaddr
 msg['To'] = toaddr
-msg['Subject'] = "GEO Data Sets - Supported platforms/corrected link"
+msg['Subject'] = "GEO Data Sets - Supported platforms"
 
-# send txt file in email body
-f6 = (open("email_data.txt",'rU'))
-geo = MIMEText(f6.read(),'plain') 
-f6.close()
+# To send send txt file in email body.
+f1 = (open("email_data.txt",'rU'))
+geo = MIMEText(f1.read(),'plain') 
+f1.close()
 msg.attach(geo)
 
-#convert to strin
+# Convert to string.
 import smtplib
 server = smtplib.SMTP('smtp.gmail.com', 587)
 server.ehlo()
 server.starttls()
 server.ehlo()
-server.login("geo.weekly.analysis", "geoweekly")
+server.login("geo.weekly.analysis", "password")
 text = msg.as_string()
 server.sendmail(fromaddr, toaddr, text)
